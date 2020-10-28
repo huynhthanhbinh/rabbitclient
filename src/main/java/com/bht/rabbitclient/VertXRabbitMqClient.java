2bd79e21d697df8d56e804fa2e7fb219ad07d8f8
binh.huynh1 <binh.huynh1@mservice.com.vn>
2020-10-28 10:18:02 +0700

    set default logging framework to log4j2 instead of slf4j (references: https://vertx.io/docs/vertx-core/java/#_using_another_logging_framework)

--- src/main/java/com/bht/rabbitclient/VertXRabbitMqClient.java
+++ src/main/java/com/bht/rabbitclient/VertXRabbitMqClient.java
@@ -1,5 +1,10 @@
 package com.bht.rabbitclient;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import io.netty.util.internal.logging.Log4J2LoggerFactory;
 import io.vertx.core.Vertx;
 
 /**
@@ -8,7 +13,11 @@ import io.vertx.core.Vertx;
  */
 public class VertXRabbitMqClient {
 
+    private static final Logger log = LogManager.getLogger(VertXRabbitMqClient.class);
+
     public static void main(String[] args) {
+        InternalLoggerFactory.setDefaultFactory(Log4J2LoggerFactory.INSTANCE);
+        log.info("Starting RabbitMQ Client v1.0 by binh.huynh1");
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(VertXRESTVerticle.class.getCanonicalName());
     }
